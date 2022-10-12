1d3e324715a93580588bf1657af65b8648ed1a48
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-10-12 21:49:40 +0900

    useState 개념 정리(완료)

--- App.js
+++ App.js
@@ -24,7 +24,8 @@ function Item(props) {
 // <Hello /> 새로 작성한 함수 호출하는 방법
 // 데이터를 줄 수 있다.
 // 작성요령 <Item image="1" title="219화" />
-// 객체,배열,변수 등 표시할 때 반드시 표시 : 중괄호{number}
+// 객체,변수 등 표시할 때 반드시 표시 : 중괄호{number}
+// 배열 : []
 
 function App() {
   // Destructring(구조 분해 할당)
@@ -55,13 +56,14 @@ function App() {
 
   // 구조 분해 할당 전 기본 코드
   // useState 규칙
-  // 1. 기존 데이터 불변성(얕은 복사, ...변수 활용)
-  // 2.
+  // 1. 기존 데이터 불변성(얕은 복사, ...변수 활용) ★★★★★★★★
   const test = React.useState();
   // console.log(test);
   //(2) [undefined, ƒ]
   //0  :   undefined
   //1  :   ƒ ()
+
+  const [text, setText] = React.useState("");
 
   // 유니크 key 값은 최상위 부모에게 줘야 한다. // <li key={index}>
   // class 는 className로 작성 // <button className="button">완료</button>
@@ -72,24 +74,64 @@ function App() {
 
   const 완료처리 = (클릭한인덱스번호) => {
     const cloneTodos = [...todos];
-    cloneTodos[클릭한인덱스번호].done = true;
+    cloneTodos[클릭한인덱스번호].done = !cloneTodos[클릭한인덱스번호].done;
+    // false 면 true 로 true 면 false 로 바꾸는 코드
+
     // alert(클릭한인덱스번호);
     // todos[클릭한인덱스번호].done = true;
     setTodos(cloneTodos); // 바뀐 값을 넣어줘야 적용됨
   };
 
+  const 삭제처리 = (클릭한인덱스번호) => {
+    const cloneTodos = [...todos];
+    const newTodos = cloneTodos.filter((item, index) => {
+      return index !== 클릭한인덱스번호;
+    });
+    setTodos(newTodos); // 바뀐 값을 넣어줘야 적용됨
+  };
+
+  const 저장처리 = () => {
+    console.log(text);
+    const cloneTodos = [...todos];
+    cloneTodos.unshift({
+      // 배열 맨 앞 추가
+      title: text,
+      date: "10.12",
+      done: false,
+    });
+    // console.log(cloneTodos);
+    setTodos(cloneTodos);
+  };
+
   return (
     <div className="App">
+      <input
+        type="text"
+        className="input"
+        onChange={(event) => {
+          setText(event.target.value);
+          // console.log(event.target.value);
+          // ...변수를 사용하지 않는 이유?  예초부터 String은  불변성이라 사용할 필요가 없음
+        }}
+      />
+      <button type="button" onClick={저장처리}>
+        저장
+      </button>
+
       <ul>
         {todos.map((item, index) => {
+          const doneName = item.done === true ? "완료" : "미완료";
+          const buttonName = item.done === true ? "완료" : "미완료";
           return (
             <li key={index}>
               <span>제목 : {item.title}</span>
               <span>날짜 : {item.date}</span>
-              <span>{item.done === true ? " 완료" : " 미완료"}</span>
+              {/* <span>{item.done === true ? " 완료" : " 미완료"}</span> */}
+              <button className="blue">{doneName}</button>
               <button className="button" onClick={완료처리.bind(this, index)}>
-                완료
+                {buttonName}
               </button>
+              <button onClick={삭제처리.bind(this, index)}>삭제</button>
             </li>
           );
         })}
