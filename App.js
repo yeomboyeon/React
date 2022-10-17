50aa38c39a4a9f7976f706d7e0f375117b3ceb24
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-10-17 20:52:28 +0900

    modal 팝업창, useEffect 구현

--- App.js
+++ App.js
@@ -1,49 +1,58 @@
-import React, { useState } from "react";
-
+import React from "react";
 import "./App.css";
 
+// React 약속
+// 최상위 부모태그는 반드시 감싸져 있어야 한다.
+// 형제태그들만 있으면 안된다.
+// 부모태그용 : <> </> 이렇게 써줘도 된다.
+/**<>
+ * <div></div>
+ * <div></div>
+ * </>
+ */
+
 function App() {
-  const [list, setList] = React.useState([
-    {
-      id: 1,
-      title: "리액트 공부",
-    },
-    {
-      id: 2,
-      title: "자바스트립트 공부",
-    },
-    {
-      id: 3,
-      title: "html 공부",
-    },
-    {
-      id: 4,
-      title: "css 공부",
-    },
-  ]);
+  const [showModal, setShowModal] = React.useState(false);
 
-  let [test, setTest] = React.useState(0);
+  // 클릭 이벤트 생성
+  React.useEffect(() => {
+    window.addEventListener("click", (event) => {
+      if (event.target.className === "modal-bg") {
+        setShowModal(false);
+      }
+    });
+  });
 
-  const 숫자올리기 = () => {
-    setTest((test += 1));
-  };
+  // showModal 바뀔 때마다 콘솔창 출력
+  React.useEffect(() => {
+    console.log("showModal 추가");
+  }, [showModal]);
+
   return (
-    // 삼항연산자 활용
-    // 버튼 10번이상 눌렸을 때 값 바꾸게 하기
-    <div className="App">
-      <header className="App-header">
-        {test >= 10 ? <p>test 값이 10 이상입니다.</p> : <p>Number : {test} </p>}
-        <button style={{ marginTop: 10 }} type="button" onClick={숫자올리기}>
-          버튼
-        </button>
-        {list.map((item, index) => {
-          return (
-            <div key={item.id}>
-              <h2>{item.title}</h2>
-            </div>
-          );
-        })}
-      </header>
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+
+        height: "100vh",
+        alignItems: "center",
+      }}
+    >
+      <button
+        onClick={() => {
+          setShowModal(true);
+        }}
+      >
+        팝업오픈버튼
+      </button>
+      {showModal && (
+        <>
+          <div className="modal-bg">modal-bg</div>
+          <div className="modal">modal</div>
+        </>
+      )}
     </div>
   );
 }
