8976763277dc73d2b08d818b1eb0e65fc4a963f7
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-10-18 16:31:35 +0900

    상품 장바구니 담고 빼기 구현

--- App.js
+++ App.js
@@ -1,56 +1,164 @@
 import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
 
-// React 약속
-// 최상위 부모태그는 반드시 감싸져 있어야 한다.
-// 형제태그들만 있으면 안된다.
-// 부모태그용 : <> </> 이렇게 써줘도 된다.
-/**<>
- * <div></div>
- * <div></div>
- * </>
- */
-
 function App() {
-  const [showModal, setShowModal] = React.useState(false);
+  const [products, setProducts] = React.useState([
+    {
+      name: "Nike CruzrOne",
+      image:
+        "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/cruzrone-unisex-shoe-T2rRwS-removebg-preview.png",
+      descrition:
+        "Designed for steady, easy-paced movement, the Nike CruzrOne keeps you going. Its rocker-shaped sole and plush, lightweight cushioning let you move naturally and comfortably. The padded collar is lined with soft wool, adding luxury to every step, while mesh details let your foot breathe. There’s no finish line—there’s only you, one step after the next.",
+      price: "20000",
+    },
+    {
+      name: "Nike Epic React Flyknit 2",
+      image:
+        "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/epic-react-flyknit-2-mens-running-shoe-2S0Cn1-removebg-preview.png",
+      descrition:
+        "Designed for steady, easy-paced movement, the Nike CruzrOne keeps you going. Its rocker-shaped sole and plush, lightweight cushioning let you move naturally and comfortably. The padded collar is lined with soft wool, adding luxury to every step, while mesh details let your foot breathe. There’s no finish line—there’s only you, one step after the next.",
+      price: "20000",
+    },
+  ]);
 
-  // 클릭 이벤트 생성
+  const [myCart, setMyCart] = React.useState([]);
+
+  const [showModal, setShowModal] = React.useState({
+    show: false,
+    image: null,
+  });
+
   React.useEffect(() => {
     window.addEventListener("click", (event) => {
       if (event.target.className === "modal-bg") {
-        setShowModal(false);
+        const cloneShowModal = { ...showModal };
+        cloneShowModal.show = false;
+        cloneShowModal.image = null;
+        setShowModal(cloneShowModal);
       }
     });
-  });
-
-  // showModal 바뀔 때마다 콘솔창 출력
-  React.useEffect(() => {
-    console.log("showModal 추가");
-  }, [showModal]);
+  }, []);
 
   return (
-    <div
-      className="App"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
+    <div className="App">
+      <div className="wrapper">
+        <div className="screen -left">
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/pngwave.png"
+            className="logo"
+            alt="로고"
+          />
+          <div className="title">Picked items</div>
+          <div className="shop-items">
+            {products &&
+              products.map((item, index) => {
+                return (
+                  <div key={`products-${index}`}>
+                    <div className="item">
+                      <div className="item-block">
+                        <div className="image-area">
+                          <img
+                            onClick={() => {
+                              const cloneShowModal = { ...showModal };
+                              cloneShowModal.show = true;
+                              cloneShowModal.image = item.image;
+                              setShowModal(cloneShowModal);
+                            }}
+                            src={item.image}
+                            alt="상품이미지"
+                            className="image"
+                          />
+                        </div>
+                        <div className="name">{item.name}</div>
+                        <div className="description">{item.descrition}</div>
+                        <div className="bottom-area">
+                          <div className="price">{item.price}</div>
+                          <div
+                            className="button"
+                            onClick={() => {
+                              // myCart 배열에 똑같은 상품이 있는지 확인
+                              // 똑같은 상품있으면 push X
+                              const cloneMyCart = [...myCart];
+                              cloneMyCart.push(item);
+                              setMyCart(cloneMyCart);
+                            }}
+                          >
+                            <p>ADD TO CART</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
 
-        height: "100vh",
-        alignItems: "center",
-      }}
-    >
-      <button
-        onClick={() => {
-          setShowModal(true);
-        }}
-      >
-        팝업오픈버튼
-      </button>
-      {showModal && (
+        <div className="screen -right">
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/pngwave.png"
+            className="logo"
+            alt="로고"
+          />
+          <div className="title">Your Cart</div>
+          <div className="shop-items">
+            {myCart ? (
+              myCart.map((item, index) => {
+                return (
+                  <div key={`products-${index}`}>
+                    <div className="item">
+                      <div className="item-block">
+                        <div className="image-area">
+                          <img
+                            onClick={() => {
+                              const cloneShowModal = { ...showModal };
+                              cloneShowModal.image = item.image;
+                              cloneShowModal.show = true;
+                              setShowModal(cloneShowModal);
+                            }}
+                            src={item.image}
+                            alt="상품이미지"
+                            className="image"
+                          />
+                        </div>
+                        <div className="name">{item.name}</div>
+                        <div className="description">{item.descrition}</div>
+                        <div className="bottom-area">
+                          <div className="price">{item.price}</div>
+                          <div className="button">
+                            <p>ADD TO CART</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })
+            ) : (
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  minHeight: 300,
+                  fontSize: 22,
+                  fontWeight: "bold",
+                }}
+              >
+                없습니다.
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+
+      {showModal.show && (
         <>
-          <div className="modal-bg">modal-bg</div>
-          <div className="modal">modal</div>
+          <div className="modal-bg" />
+          <div className="modal">
+            <img src={showModal.image} alt="확대사진" />
+          </div>
         </>
       )}
     </div>
