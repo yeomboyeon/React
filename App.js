571d47aef83744fea73d66e35bbdf9933cfddbdd
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-10-26 14:48:11 +0900

    useEffect 숙달편(기초)
    - 콜백함수
    - dependency array

--- App.js
+++ App.js
@@ -1,48 +1,56 @@
 import "./App.css";
-import { useState } from "react"; // 추가
+import { useState, useEffect } from "react"; // 추가
 
-const work = () => {
-  console.log("무거운 작업들");
-  return ["염보연", "문연정"];
-};
+/**
+useEffect(( ) => { // 작업할 코드 }, [value]);
+* 콜백함수 + 배열(dependency array)
+* 값이 변할때에만 실행되도록 할려면 [ ] 추가
+* 해당 값만 실행되기에 효율적임
+
+useEffect(( ) => { // 작업할 코드 }, [  ]);
+* 첫 랜더링 할때만 실행
+ */
 
 function App() {
-  // 처음에만 실행되도록 콜백 함수로 추가
-  const [names, setNames] = useState(() => {
-    return work();
+  const [count, setCount] = useState(1);
+  const [name, setName] = useState("");
+
+  const handleCountUpdate = () => {
+    setCount(count + 1);
+  };
+
+  const handleInputChange = (e) => {
+    setName(e.target.value);
+  };
+
+  console.log(count);
+
+  // 랜더링 될 때마다 실행
+  useEffect(() => {
+    console.log("랜더링");
   });
 
-  // const [names, setNames] = useState(["염보연", "문연정"]); // 초기값 (여러 문자열 넣을 때에는 [] 추가)
-  //        현재상태, 변경값               초기값 설정(콜백 함수로 가능)
+  // 처음 랜더링 될 때만 실행
+  useEffect(() => {
+    console.log("랜더링");
+  }, []);
 
-  const [input, setInput] = useState(""); // 업데이트 할 때마다 담을 수 있도록 추가(빈 문자열로)
+  // 마운트 + [해당값] 만 변경 될 때마다 실행
+  useEffect(() => {
+    console.log("랜더링");
+  }, [count]);
 
-  // 인자 e / 이벤트. 이벤트안에 있는 타겟에 값
-  const handleInputChange = (e) => {
-    setInput(e.target.value);
-  };
+  // 마운트 + [해당값] 만 변경 될 때마다 실행
+  useEffect(() => {
+    console.log("랜더링");
+  }, [name]);
 
-  // 업로드 함수
-  // setNames 안에 콜백함수로 넣고 이전 값 ...prevState 으로 받아오기
-  // 인자에는 이전 값
-  const handleUpload = () => {
-    setNames((prevState) => {
-      console.log("이전 State : ", prevState); // 콘솔창 확인
-      return [input, ...prevState];
-    });
-  };
-
-  console.log(input); // 콘솔창 확인
-
-  // map 에는 엘리멘트 추가하기 위해서 key 값 작성 필, index 작성 추가
-  // 사용자가 입력할 때마다 호출할 수 있도록 onChange 추가
   return (
     <div>
-      <input type="text" value={input} onChange={handleInputChange} />
-      <button onClick={handleUpload}>Upload</button>
-      {names.map((name, index) => {
-        return <p key={index}>{name}</p>;
-      })}
+      <button onClick={handleCountUpdate}>Update</button>
+      <span>count : {count}시</span>
+      <input type="text" value={name} onChange={handleInputChange} />
+      <span>name : {name}</span>
     </div>
   );
 }
