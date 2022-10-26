92e340566b8121b068a438b2348362619d7bf2f0
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-10-26 21:01:50 +0900

    카카오프렌즈 MBTI 채팅창 구현(첫화면)

--- App.js
+++ App.js
@@ -1,19 +1,53 @@
 import "./App.css";
-import { useState, useEffect } from "react"; // 추가
-import Timer from "./component/Timmer";
-/**
-클린 업
- */
+
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+function Main() {
+  const navigation = useNavigate();
+
+  return (
+    <div className="main-app">
+      <img
+        src="https://kakaofriendsmbti.netlify.app/static/media/00.88f71908.png"
+        alt="메인이미지"
+      />
+      <button
+        className="btn"
+        type="button"
+        onClick={() => {
+          navigation("/on1");
+        }}
+      >
+        시작하기
+      </button>
+    </div>
+  );
+}
+
+function On1() {
+  return (
+    <div className="main-app">
+      <div className="progress-bar">
+        <div className="percent"></div>
+      </div>
+      <div className="image-box">
+        <img
+          src="https://kakaofriendsmbti.netlify.app/images/01-01.png"
+          alt="온보딩이미지"
+        />
+      </div>
+      <button className="btn">당연하지 어디서 할지 고민중이야!</button>
+      <button className="btn">그냥 맛있는거 먹으로 갈까 생각 중이야!</button>
+    </div>
+  );
+}
 
 function App() {
-  const [showTimer, setShowTimer] = useState(false);
-
-  // showTimer 참일때만 보여주기 &&
   return (
-    <div>
-      {showTimer && <Timer />}
-      <button onClick={() => setShowTimer(!showTimer)}>Toggle Timer</button>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Main />} />
+      <Route exact path="/on1" element={<On1 />} />
+    </Routes>
   );
 }
 
