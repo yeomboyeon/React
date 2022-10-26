a0a24affb4a30452c9b17e33dcfb5568eced9cfd
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-10-26 14:28:27 +0900

    useState 숙달편(기초)(최종)
    - prevState
    - map 
    추가

--- App.js
+++ App.js
@@ -1,28 +1,48 @@
 import "./App.css";
 import { useState } from "react"; // 추가
 
-function App() {
-  const [time, setTime] = useState(1); // [변수값 설정 가능], 초기값 작성
+const work = () => {
+  console.log("무거운 작업들");
+  return ["염보연", "문연정"];
+};
 
-  // 클릭할 때마다 타임 1씩 증가
-  // if 문 통해서 12시가 넘어가면 다시 1시로 바뀌도록 구현
-  const handleClick = () => {
-    let newTime;
-    if (time >= 12) {
-      newTime = 1;
-    } else {
-      newTime = time + 1;
-    }
-    setTime(newTime);
+function App() {
+  // 처음에만 실행되도록 콜백 함수로 추가
+  const [names, setNames] = useState(() => {
+    return work();
+  });
+
+  // const [names, setNames] = useState(["염보연", "문연정"]); // 초기값 (여러 문자열 넣을 때에는 [] 추가)
+  //        현재상태, 변경값               초기값 설정(콜백 함수로 가능)
+
+  const [input, setInput] = useState(""); // 업데이트 할 때마다 담을 수 있도록 추가(빈 문자열로)
+
+  // 인자 e / 이벤트. 이벤트안에 있는 타겟에 값
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
   };
 
-  // 랜더링되는지 여부 확인
-  console.log("업데이트");
+  // 업로드 함수
+  // setNames 안에 콜백함수로 넣고 이전 값 ...prevState 으로 받아오기
+  // 인자에는 이전 값
+  const handleUpload = () => {
+    setNames((prevState) => {
+      console.log("이전 State : ", prevState); // 콘솔창 확인
+      return [input, ...prevState];
+    });
+  };
 
+  console.log(input); // 콘솔창 확인
+
+  // map 에는 엘리멘트 추가하기 위해서 key 값 작성 필, index 작성 추가
+  // 사용자가 입력할 때마다 호출할 수 있도록 onChange 추가
   return (
     <div>
-      <span>현재시각 : {time}시</span>
-      <button onClick={handleClick}>upDate</button>
+      <input type="text" value={input} onChange={handleInputChange} />
+      <button onClick={handleUpload}>Upload</button>
+      {names.map((name, index) => {
+        return <p key={index}>{name}</p>;
+      })}
     </div>
   );
 }
