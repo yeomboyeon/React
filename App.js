13898f372954dd9d3804bf8a6912d27a5f083658
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-11-02 22:18:50 +0900

    카카오 프렌즈 mbti(서버 연동, 결과화면 추가, 임시 저장 기능 구현중)

--- App.js
+++ App.js
@@ -1,34 +1,30 @@
+import React from "react";
 import "./App.css";
 import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
-import React from "react";
 import axios from "axios";
 
 // 1. import 추가 ({ Routes, Route }, "react-router-dom")
 // 2. App Routes, Route 실행 경로 및 함수 추가
-// 3. Main, on1 등 구현 내용에 맞게 추가 (*** 대문자로 시작)
+// 3. Main, sub 등 구현 내용에 맞게 추가 (*** 대문자로 시작)
 // 4. css 꾸며주기
-// 5. navigation(네비게이션) 함수, import useNavigate 추가
+// 5. navigation(네비게이션) 함수, import 추가
 // 6. navigation 버튼을 눌렀을 때 해당 화면으로 이동하도록 onClick에 경로('/on1')추가
-// 7. 이미지창, 하단 결과창, 페이지별 퍼센트박스크기 자동으로 변경되도록 구현하기 (props)
+// 7. 페이지별 퍼센트박스크기 자동으로 변경되도록 구현하기 (props)
 // 8. compornent 화하기 (함수, 경로 추가)(같은 태그를 묶기)(가독성 향상)(props로 주고받기)
-// 9. useContext() 구현
-// 10. dispatch, mbti 로직 구현
-// 11. mbti state 값 바꾸는 로직 구현(useEffect())
-// 12. 결과창 구현(useLocation()) import useLocation 추가 (현재 url 정보 가져오기)
-// 13. 서버 연동(axios) import axios 추가
 
-// 상단 퍼센트창(props)
+// 상단 퍼센트창
 const ProgressBar = (props) => {
+  // (총길이 / 총 페이지(스탭)수) * 받아오는 스텝(페이지)
   const width = (400 / 5) * props.step;
 
   return (
-    <div className="percent-Bar">
+    <div className="progress-bar">
       <div className="percent" style={{ width: width }}></div>
     </div>
   );
 };
 
-// 이미지창(props)
+// 결과 이미지창
 const Question = (props) => {
   return (
     <div className="image-box">
@@ -37,12 +33,19 @@ const Question = (props) => {
   );
 };
 
-// 하단 결과창 (props)
+// 하단 버튼창
+// useContext 활용
 const Answer = (props) => {
-  const navigation = useNavigate(); // 페이지 이동하기 위함
+  const navigation = useNavigate();
 
   const { setDispatchType } = React.useContext(StoreContext);
 
+  // 주소 경로 찾아서 보여주기(window.location.pathname)
+  // charAt(length-1) 해당 위치 인덱스 가져오기 - 마지막 인덱스 가져오기
+  // 현재 스탭 - pathname.charAt(pathname.length - 1)
+  // 다음 스탭 - pathname.charAt(pathname.length - 1) + 1
+  // 문자열 취급이기에 숫자로 변환 처리(parseInt로 묶기)
+  // 비지니스 로직이라고 칭함(onClick에 쓰인 함수)
   return (
     <button
       className="btn"
@@ -50,9 +53,15 @@ const Answer = (props) => {
         setDispatchType({
           code: "답변",
           params: {
-            value: props.value,
+            value: props.value, // 클릭한 값
           },
         });
+
+        // console.log(`내가 누른 값 ${props.value}, cloneMbti`);
+        // const pathname = window.location.pathname;
+        // const nextStep = parseInt(pathname.charAt(pathname.length - 1) + 1);
+        // // console.log(nextStep); // 11(문자+숫자로 표기됨)
+        // navigation(`/on${nextStep}`);
       }}
     >
       {props.text}
@@ -60,9 +69,10 @@ const Answer = (props) => {
   );
 };
 
+// compornent 작업
 function On1() {
   return (
-    <div className="main-App">
+    <div className="main-app">
       <ProgressBar step={1} />
       <Question image="https://kakaofriendsmbti.netlify.app/images/01-01.png" />
       <Answer text="당연하지! 어디서 할지 고민 중이야!" value="E" />
@@ -73,10 +83,10 @@ function On1() {
 
 function On2() {
   return (
-    <div className="main-App">
+    <div className="main-app">
       <ProgressBar step={2} />
       <Question image="https://kakaofriendsmbti.netlify.app/images/02-01.png" />
-      <Answer text="영화 완전 재미었어! 너도 한번 봐봐!" value="S" />
+      <Answer text="영화 완전 재미있어! 너도 한번 봐봐!" value="S" />
       <Answer
         text="좀비가 너무 리얼했어. 실제 상황이면 난 바로 죽었을거야..."
         value="N"
@@ -87,84 +97,103 @@ function On2() {
 
 function On3() {
   return (
-    <div className="main-App">
+    <div className="main-app">
       <ProgressBar step={3} />
       <Question image="https://kakaofriendsmbti.netlify.app/images/03-01.png" />
-      <Answer
-        text="지금 PPT 만드는 중이니까 아마 한 2시간 뒤면 끝날거 같아!"
-        value="T"
-      />
-      <Answer text="모르겠어. 근데 지금 PPT 만들고 있어!" value="F" />
+      <Answer text="무슨 꽃 샀어? 향은 좋아?" value="T" />
+      <Answer text="왜 우울해? 무슨 일 있어?" value="F" />
     </div>
   );
 }
 
 function On4() {
   return (
-    <div className="main-App">
+    <div className="main-app">
       <ProgressBar step={4} />
       <Question image="https://kakaofriendsmbti.netlify.app/images/04-01.png" />
       <Answer
-        text="그래! 역시 사람 많고 유명한 벚꽃 명소가 예쁘겠지 어디로 갈까?"
+        text="지금 PPT 만드는 중이니까 아마 한 2시간 뒤면 끝날거 같아!"
         value="J"
       />
-      <Answer text="그래! 사람 적은 벚꽃 명소 한 번 찾아볼까?" value="P" />
+      <Answer text="모르겠어. 근데 지금 PPT 만들고 있어!" value="P" />
     </div>
   );
 }
 
 function On5() {
   return (
-    <div className="main-App">
+    <div className="main-app">
       <ProgressBar step={5} />
       <Question image="https://kakaofriendsmbti.netlify.app/images/05-01.png" />
-      <Answer text="지구는 멸망하지 않아!" value="E" />
-      <Answer text="일단 가장 좋아하는 음식부터 먹으러 갈거야!" value="I" />
+      <Answer
+        text="그래! 역시 사람 많고 유명한 벚꽃 명소가 예쁘겠지 어디로 갈까?"
+        value="E"
+      />
+      <Answer text="그래! 사람 적은 벚꽃 명소 한 번 찾아볼까?" value="I" />
     </div>
   );
 }
 
 function Result() {
-  // state(mbti) 값을 변수로 잡기
+  const navigation = useNavigate();
+
   const { state } = useLocation();
   // console.log(state);
 
-  // axios 서버 연동하기
-  const MBTI결과가져오기 = () => {
-    axios({
-      url: "http://localhost:4000/mbti",
-      method: "get", // 기본 타입으로 생락 가능
-      responseType: "json", // 기본 타입으로 생락 가능
+  // 데이터 저장하기
+  const [result, setResult] = React.useState(undefined);
+
+  // axios
+  const MBTI결과가져오기 = async () => {
+    await axios({
+      url: "http://localhost:5000/mbti",
       params: state,
     })
-      .than(() => {})
-      .catch((e) => {
-        console.log("에러", e);
+      .than(({ data }) => {
+        // console.log(data);
+        setResult(data);
+      })
+      .catch((err) => {
+        console.log("에러", err);
       });
   };
 
   React.useEffect(() => {
     MBTI결과가져오기();
-  });
+  }, []);
 
-  console.log(MBTI결과가져오기);
+  // console.log(MBTI결과가져오기);
 
-  return <div>결과창</div>;
+  // 에러 방지 위해서
+  if (result === undefined) {
+    return <div></div>;
+  }
+
+  return (
+    <div>
+      <img className="result-img" src={result.content} alt="결과화면" />
+      <button
+        className="btn"
+        onClick={() => {
+          navigation("/on1");
+        }}
+      ></button>
+    </div>
+  );
 }
 
 function Main() {
   const navigation = useNavigate();
 
   return (
-    <div className="main-App">
-      <div className="image-box">
-        <img
-          src="https://kakaofriendsmbti.netlify.app/static/media/00.88f71908.png"
-          alt="메인이미지"
-        />
-      </div>
+    <div className="main-app">
+      <img
+        src="https://kakaofriendsmbti.netlify.app/static/media/00.88f71908.png"
+        alt="메인이미지"
+      />
       <button
         className="btn"
+        type="button"
         onClick={() => {
           navigation("/on1");
         }}
@@ -177,14 +206,30 @@ function Main() {
 
 const StoreContext = React.createContext({});
 
+// useContext 활용
+// React useReducer(비지니스 로직 분리되어 있는걸 한곳에 모음)
 function App() {
   const navigation = useNavigate();
 
+  // 임시 저장 기능 값 가져와서 보여주기
+  React.useEffect(() => {
+    let a = JSON.parse(localStorage.getItem("mbti"));
+
+    if (window.confirm("저번에 하던게 있어요 이어서 하실래요?")) {
+      console.log(a);
+    } else {
+      localStorage.removeItem("mbti");
+    }
+  });
+
+  // 모든 컴포넌트(경로)에서 사용할 수 있도록
+  // mbti 값을 추가로 보내지 않고 디스패치 하나로 모든 값을 한번에 적용되도록 하기 위함
   const [dispatch, setDispatchType] = React.useState({
     code: null,
     params: null,
   });
 
+  // 내가 답변한 mbti 값 저장해놓을 곳
   const [mbti, setMbti] = React.useState([
     {
       I: 0, // 내향
@@ -204,9 +249,6 @@ function App() {
     },
   ]);
 
-  // 페이지 이동(초기값 1페이지)
-  let [page, setPage] = React.useState(1);
-
   // // 버튼을 누를때마다 setMbti 를 통해 값 변경
   // // findIndex 활용 index 값을 찾기
   // const findIndex = setMbti.findIndex((item) => {
@@ -214,41 +256,51 @@ function App() {
   //   return item == setMbti;
   // });
 
+  //페이지 이동
+  let [page, setPage] = React.useState(1);
+
+  //mbti state 값 바꾸는 로직 구현(의존) * 한번 실행하고 끝.
   React.useEffect(() => {
     // console.log(dispatch);
     switch (dispatch.code) {
       case "답변":
-        // const { value } = dispatch.params;
         const cloneMbti = [...mbti];
         const findIndex = cloneMbti.findIndex((item) => {
           return item[dispatch.params.value] !== undefined;
         });
 
-        // console.log(cloneMbti[findIndex][dispatch.params.value]);
         cloneMbti[findIndex][dispatch.params.value] += 1;
         setMbti(cloneMbti);
+        // console.log(cloneMbti[findIndex][dispatch.params.value]);
 
+        // 클릭해서 답변 담길 때 페이지 이동
         const nextPage = (page += 1);
+
         setPage(nextPage);
-        // console.log(nextPage);
+
+        // 임시저장 기능 구현중
+        const JSON문자열 = JSON.stringify(mbti);
+        localStorage.setItem("mbti", JSON문자열);
 
         if (nextPage === 6) {
           navigation("/result", {
             state: mbti,
           });
         } else {
-          navigation(`/on${nextPage}`);
+          navigation(`/on${nextPage}`); // 경로 주소에 맞게 작성(대소문자 구분)
         }
-
         break;
 
       default:
         break;
     }
+    // 의존성(dispatch 실행될 때마다 함수 실행)
   }, [dispatch]);
 
-  // StoreContext.Provider 통해서 모든 컴포넌트에서 활용 가능
+  // StoreContext 사용을 통해 setMbti 전 경로에서 사용 가능
+  //
   return (
+    // mbti를 여기에 줘도 되나, 결과창에만 보여주기에 낭비여서 주지 않음
     <StoreContext.Provider value={{ setDispatchType }}>
       <Routes>
         <Route exact path="/" element={<Main />}></Route>
